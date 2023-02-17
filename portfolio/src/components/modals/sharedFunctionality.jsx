20991cfa20bf6fa967d7924105f931c595b5c90c
Lyndsi Kay Williams <kcatgirl@gmail.com>
2023-02-17 13:50:37 -0600

    Fix CI

--- portfolio/src/components/modals/sharedFunctionality.jsx
+++ portfolio/src/components/modals/sharedFunctionality.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
