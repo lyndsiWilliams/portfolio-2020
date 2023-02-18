154d902038ec6059a3d2f6185b1ce0a13a4dadec
Lyndsi Kay Williams <kcatgirl@gmail.com>
2023-02-18 14:39:51 -0600

    Modal cleanup test

--- portfolio/src/components/modals/TeamLead.js
+++ portfolio/src/components/modals/TeamLead.js
@@ -1,40 +1,126 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import { DialogContent, DialogTitle } from './sharedFunctionality';
+import React from "react";
+// import Button from "@material-ui/core/Button";
+// import Dialog from "@material-ui/core/Dialog";
+// import Modal, { DialogContent, DialogTitle } from "./sharedFunctionality";
+import Modal from "./sharedFunctionality";
 
 export default function TeamLead() {
-  const [open, setOpen] = React.useState(false);
+  // const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+  // const handleClickOpen = () => {
+  //   setOpen(true);
+  // };
+  // const handleClose = () => {
+  //   setOpen(false);
+  // };
 
   return (
-    <div style={{ marginTop: "2%" }}>
-      <Button data-testid="TLmodal" variant="contained" color="secondary" onClick={handleClickOpen} style={{ fontFamily: "Kufam", borderWidth: '4px' }}>
-        • Bloom Institute of Technology (formerly Lambda School) •<br/>Team Lead
-      </Button>
-      <Dialog onClose={handleClose} open={open}>
-        <DialogTitle onClose={handleClose} >
-          <span style={{ fontFamily: "'Raleway', sans-serif", fontWeight: 'bold' }}>Bloom Institute of Technology (formerly Lambda School)<br/>March 2020 - June 2020</span>
-        </DialogTitle>
-        <DialogContent dividers>
-          <div className="experience-bullets">
-            <p style={{ textAlign: "center", fontFamily: "'Raleway', sans-serif", fontSize: '1.5em', fontWeight: 'bold' }}>Team Lead - Remote</p>
-            <p>• Provided daily feedback and issue resolution of all curriculum through Slack, help channel, and/or code review.</p>
+    <>
+      <Modal
+        buttonText={`• Bloom Institute of Technology (formerly Lambda School) •${(
+          <br />
+        )}Team Lead`}
+        modalTitle={`Bloom Institute of Technology (formerly Lambda School)${(
+          <br />
+        )}March 2020 - June 2020`}
+        contentTitle={`Team Lead - Remote`}
+        contentDescription={
+          <>
+            <p>
+              • Provided daily feedback and issue resolution of all curriculum
+              through Slack, help channel, and/or code review.
+            </p>
             <p>• Delivered 1:1 feedback and code review on a weekly basis</p>
-            <p>• Led a team of 7-9 students that emulates a real work environment using Agile methodology</p>
-            <p>• Mentored a team of 7-9 students on how to learn effectively and solve programming problems</p>
-            <p>• Reviewed student code and provided detailed feedback on areas of improvement</p>
-            <p>• Facilitated daily standup meetings to build camaraderie, encourage sharing of ideas and work progress, and provide guidance for that week's study material</p>
-            <p>• Delivered supplemental lectures on various topics related to software engineering</p>
-          </div>
-        </DialogContent>
-      </Dialog>
-    </div>
+            <p>
+              • Led a team of 7-9 students that emulates a real work environment
+              using Agile methodology
+            </p>
+            <p>
+              • Mentored a team of 7-9 students on how to learn effectively and
+              solve programming problems
+            </p>
+            <p>
+              • Reviewed student code and provided detailed feedback on areas of
+              improvement
+            </p>
+            <p>
+              • Facilitated daily standup meetings to build camaraderie,
+              encourage sharing of ideas and work progress, and provide guidance
+              for that week's study material
+            </p>
+            <p>
+              • Delivered supplemental lectures on various topics related to
+              software engineering
+            </p>
+          </>
+        }
+      />
+      {/* <div style={{ marginTop: "2%" }}>
+        <Button
+          data-testid="TLmodal"
+          variant="contained"
+          color="secondary"
+          onClick={handleClickOpen}
+          style={{ fontFamily: "Kufam", borderWidth: "4px" }}
+        >
+          • Bloom Institute of Technology (formerly Lambda School) •<br />
+          Team Lead
+        </Button>
+        <Dialog onClose={handleClose} open={open}>
+          <DialogTitle onClose={handleClose}>
+            <span
+              style={{
+                fontFamily: "'Raleway', sans-serif",
+                fontWeight: "bold",
+              }}
+            >
+              Bloom Institute of Technology (formerly Lambda School)
+              <br />
+              March 2020 - June 2020
+            </span>
+          </DialogTitle>
+          <DialogContent dividers>
+            <div className="experience-bullets">
+              <p
+                style={{
+                  textAlign: "center",
+                  fontFamily: "'Raleway', sans-serif",
+                  fontSize: "1.5em",
+                  fontWeight: "bold",
+                }}
+              >
+                Team Lead - Remote
+              </p>
+              <p>
+                • Provided daily feedback and issue resolution of all curriculum
+                through Slack, help channel, and/or code review.
+              </p>
+              <p>• Delivered 1:1 feedback and code review on a weekly basis</p>
+              <p>
+                • Led a team of 7-9 students that emulates a real work
+                environment using Agile methodology
+              </p>
+              <p>
+                • Mentored a team of 7-9 students on how to learn effectively
+                and solve programming problems
+              </p>
+              <p>
+                • Reviewed student code and provided detailed feedback on areas
+                of improvement
+              </p>
+              <p>
+                • Facilitated daily standup meetings to build camaraderie,
+                encourage sharing of ideas and work progress, and provide
+                guidance for that week's study material
+              </p>
+              <p>
+                • Delivered supplemental lectures on various topics related to
+                software engineering
+              </p>
+            </div>
+          </DialogContent>
+        </Dialog>
+      </div> */}
+    </>
   );
-};
+}
--- portfolio/src/components/modals/sharedFunctionality.jsx
+++ portfolio/src/components/modals/sharedFunctionality.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
 import IconButton from '@material-ui/core/IconButton';
@@ -24,11 +26,11 @@ export const DialogTitle = withStyles(styles)((props) => {
   return (
     <MuiDialogTitle disableTypography className={classes.root} {...other}>
       <Typography variant="h6">{children}</Typography>
-      {onClose ? (
+      {onClose ?? (
         <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
           <CloseIcon />
         </IconButton>
-      ) : null}
+      )}
     </MuiDialogTitle>
   );
 });
@@ -38,3 +40,35 @@ export const DialogContent = withStyles((theme) => ({
     padding: theme.spacing(2),
   },
 }))(MuiDialogContent);
+
+const Modal = (buttonText, modalTitle, contentTitle, contentDescription) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div style={{ marginTop: "2%" }}>
+      <Button data-testid="TLmodal" variant="contained" color="secondary" onClick={handleClickOpen} style={{ fontFamily: "Kufam", borderWidth: '4px' }}>
+        {buttonText}
+      </Button>
+      <Dialog onClose={handleClose} open={open}>
+        <DialogTitle onClose={handleClose} >
+          <span style={{ fontFamily: "'Raleway', sans-serif", fontWeight: 'bold' }}>{modalTitle}</span>
+        </DialogTitle>
+        <DialogContent dividers>
+          <div className="experience-bullets">
+            <p style={{ textAlign: "center", fontFamily: "'Raleway', sans-serif", fontSize: '1.5em', fontWeight: 'bold' }}>{contentTitle}</p>
+            {contentDescription}
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default Modal;
