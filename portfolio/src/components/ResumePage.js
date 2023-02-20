dc152842857bb0b880780a852c68002f41f2f8e2
Lyndsi Kay Williams <kcatgirl@gmail.com>
2023-02-20 13:36:51 -0600

    Trying some different stuff with this iframe

--- portfolio/src/components/ResumePage.js
+++ portfolio/src/components/ResumePage.js
@@ -6,8 +6,6 @@ const ResumePage = () => {
       <iframe
         src="https://docs.google.com/document/d/e/2PACX-1vRERMPCJxpPmykMYDJBhpiWHkq2_h-846yIRP1Ivti76hCfus1pHb-azUg96dGlVGmxr-h4c6VdFp8i/pub?embedded=true"
         title="file"
-        width="100%"
-        height="600"
       ></iframe>
     </div>
   );
