d71dfe53acab63542968e86ef7447ac7f2823393
Lyndsi Kay Williams <kcatgirl@gmail.com>
2023-02-20 13:39:28 -0600

    This might be better (styling)

--- portfolio/src/components/ResumePage.js
+++ portfolio/src/components/ResumePage.js
@@ -2,10 +2,11 @@ import React from "react";
 
 const ResumePage = () => {
   return (
-    <div>
+    <div style={{ marginTop: "2%" }}>
       <iframe
         src="https://docs.google.com/document/d/e/2PACX-1vRERMPCJxpPmykMYDJBhpiWHkq2_h-846yIRP1Ivti76hCfus1pHb-azUg96dGlVGmxr-h4c6VdFp8i/pub?embedded=true"
         title="file"
+        width="80%"
       ></iframe>
     </div>
   );
