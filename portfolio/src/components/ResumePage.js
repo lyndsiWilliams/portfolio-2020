3cefb33a6447ad8a163be06eaccea3b55d7e96a6
Lyndsi Kay Williams <kcatgirl@gmail.com>
2023-02-20 14:49:35 -0600

    Teeny tiny height tweak on iframe

--- portfolio/src/components/ResumePage.js
+++ portfolio/src/components/ResumePage.js
@@ -7,7 +7,7 @@ const ResumePage = () => {
         src="https://docs.google.com/document/d/e/2PACX-1vRERMPCJxpPmykMYDJBhpiWHkq2_h-846yIRP1Ivti76hCfus1pHb-azUg96dGlVGmxr-h4c6VdFp8i/pub?embedded=true"
         title="file"
         width="80%"
-        height="626px"
+        height="620px"
         style={{ maxWidth: "834px" }}
       ></iframe>
     </div>
